a5c2e728ddb3df8eed7b99a403c5730572c0bd87
STARTIM <msg.stark2@extaccount.com>
2020-12-29 18:12:43 +0100

    simple end to end test

--- e2e_test/tests/simpleTest.ts
+++ e2e_test/tests/simpleTest.ts
@@ -1,16 +1,38 @@
-import { ui5Fixture, ui5Test } from "ui5-testcafe-selector-utils";
+import { ui5, ui5Fixture, ui5Test } from "ui5-testcafe-selector-utils";
 import productDemo from "../pages/productDemo";
 
-ui5Fixture("Startseite", "https://sapui5.hana.ondemand.com/test-resources/sap/m/demokit/cart/webapp/index.html?sap-ui-theme=sap_fiori_3");
+ui5Fixture("Shop-App", "https://sapui5.hana.ondemand.com/test-resources/sap/m/demokit/cart/webapp/index.html?sap-ui-theme=sap_fiori_3");
 
-ui5Test('Product-Demo', async u => {
-    await u.expect(productDemo.masterPage.productCatList).tableLength().greater(0, "Initially the product-list must be filled");
+ui5Test('Create and Order Shopping-Cart', async u => {
+    //add HT-1035 to shopping-cart
+    await productDemo.addToShoppingCart("Flat Basic", "HT-1035");
 
-    await u.typeText(productDemo.masterPage.searchfield, "Flat Basic");
+    //navigate to cart / checkout section
+    await u.click(productDemo.detailPage.navtoCartButton);
+    await u.expect(productDemo.cardPage.items).tableLength().equal(1);
+    await u.click(productDemo.cardPage.proceedButton);
 
-    const data = await productDemo.masterPage.productList.data();
+    //validate shopping-basket and navigate to payment
+    await u.expect(productDemo.checkoutView.items).tableLength().equal(1);
+    await u.click(productDemo.checkoutView.nextStepButton).
+        click(productDemo.paymentView.nextStepButton);
 
-    await u.expectAny(data.tableData.data[0].ProductId).equal('HT-1035', "There is only one product with Flat-Basic - this should be found");
-    await u.expect(productDemo.masterPage.productList).dynamic((e) => e.tableData.data[0].ProductId).equal("HT-1035");
-    await u.expect(productDemo.masterPage.productList).tableLength().equal(1);
+    //maintain payment -> at least once enter incorrect information
+    await productDemo.maintainCreditCart({ holder: "Max Mustermann", number: "1234", cvn: "123", expiration: "10/2022" });
+    await u.expect(productDemo.creditCartView.number).property('valueState').equal('Error');
+    await u.expectExists(productDemo.creditCartView.nextButton).notOK();
+    await productDemo.maintainCreditCart({ number: "1234567891234567" });
+    await u.expect(productDemo.creditCartView.number).property('valueState').equal('None');
+    await u.click(productDemo.creditCartView.nextButton);
+
+    //maintain address
+    await productDemo.maintainAddress({ address: "Address", city: "Munich", country: "Germany", zip: "12345" });
+    await u.click(productDemo.addressView.nextButton);
+
+    await u.click(productDemo.deliveryView.nextButton);
+
+    await u.click(productDemo.submitView.submitButton).
+        click(productDemo.submitView.confirm);
+
+    await u.expect(productDemo.completedView.confText).exists().ok();
 }); 

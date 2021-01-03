ff704b80108d77cc1bdc52d8c8497aed5d573f49
STARTIM <msg.stark2@extaccount.com>
2021-01-03 19:58:24 +0100

    correct

--- e2e_simple/first.ts
+++ e2e_simple/first.ts
@@ -2,8 +2,11 @@ import { ui5Fixture, ui5Test, ui5, ui5TraceMismatchType, ui5Action } from "ui5-t
 
 ui5Fixture("Startseite", "https://sapui5.hana.ondemand.com/test-resources/sap/m/demokit/cart/webapp/index.html?sap-ui-theme=sap_fiori_3");
 
-ui5Test('Product-Demo', async (u, t) => {
-    await u.typeText(ui5().id("homeView--searchField"), "Flat Basic");
-    await u.expect(ui5().id("homeView--productList")).tableLength().equal(1);
-    await u.expectProperty(ui5().element('sap.m.ObjectNumber').context('ProductId', 'HT-1035'), "number").equal("399,00")
+ui5Test('Product-Demo', async u => {
+    await u.typeText(ui5().id("homeView--searchField"), "Flat Basic").
+        expect(ui5().id("homeView--productList")).tableLength().equal(1);
+
+    const objectNumber = ui5().element('sap.m.ObjectNumber').context('ProductId', 'HT-1035');
+    await u.expectProperty(objectNumber, "number").equal("399,00");
+    await u.click(objectNumber);
 }); 
